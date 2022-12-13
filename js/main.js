a7982fefb77fb5691453f8f80854c2254d5fd86a
ryansurf <94500732+ryansurf@users.noreply.github.com>
2022-12-13 01:35:27 -0800

    added buttons and shadows

--- js/main.js
+++ js/main.js
@@ -24,3 +24,18 @@ function init(){
 }
 
 init();
+
+// CHANGE TEXT Function
+var text = ["Student", "Programmer", "Surfer", "Fisherman", "Backpacker", "Snowboarder"];
+var counter = 0;
+var elem = document.getElementById("changeText");
+var inst = setInterval(change, 2500);
+
+function change() {
+  elem.innerHTML = text[counter];
+  counter++;
+  if (counter >= text.length) {
+    counter = 0;
+    // clearInterval(inst); // uncomment this if you want to stop refreshing after one cycle
+  }
+}
